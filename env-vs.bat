127bb617d943ea5561789e834c3c8c6283523905
mpab <mpab@asrai.io>
2020-04-20 10:48:01 +0200

    renamed, tweaked, default is 64 bit

--- env-vs.bat
+++ env-vs.bat
@@ -10,22 +10,19 @@ rem   Determine path to VsDevCmd.bat
 rem
 
 if not "%VsDevCmd_Path%x" == "x" (
-    echo VsDevCmd_Path is already set to %VsDevCmd_Path%, exiting
+    echo VsDevCmd_Path is already set to "%VsDevCmd_Path%", exiting
     goto :eof
 )
 
 for /f "usebackq delims=#" %%a in (`"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do set VsDevCmd_Path=%%a\Common7\Tools\VsDevCmd.bat
 echo path is %VsDevCmd_Path%
 
-
-if [%1] equ [64] (
-  echo using 64 bit
-  "%VsDevCmd_Path%" -arch=amd64
-
+if "%1x" == "32x" (
+    echo using 32 bit
+    "%VsDevCmd_Path%"
 ) else (
-  echo using 32 bit
-  "%VsDevCmd_Path%"
+    echo using 64 bit
+    "%VsDevCmd_Path%" -arch=amd64
 )
 
-rem set VSCMD_DEBUG=
 set VsDevCmd_Path=
