ba50f013e8f2242eb8fdb0b343e957879d9d0456
mpab <mpab@asrai.io>
2020-05-08 21:17:47 +0200

    new pattern

--- iis-generate.bat
+++ iis-generate.bat
@@ -17,28 +17,35 @@ call :create_hosts_section
 goto :eof
 
 :create_web_site_script
-set site_name=%domain%-%env%%app%
-set site_path=C:\inetpub\%domain%\%env%%app%
-set site_bindings=%env%%host_name%.%domain%
+if "%webapp%x"=="uix" (
+    set sub_domain=%environment%-%solution%
+) else (
+    set sub_domain=%environment%-%webapp%-%solution%
+)
+
+set site_name=%domain%-%solution%-%webapp%-%environment%
+set host_name=%sub_domain%.%domain%
+set physical_path=C:\inetpub\%domain%-%solution%-%environment%\%vsproject%
+
+set site_bindings=%host_name%
 
 echo %app_cmd% delete site %site_name% >> %iis_script%
 echo %app_cmd% delete apppool %site_name% >> %iis_script%
 
-rem echo %app_cmd% add site /name:%site_name% /site.name:%site_name% /bindings:http://*:80 /physicalPath:%site_path% >> %iis_script%
-rem echo %app_cmd% add site /name:%site_name% /site.name:%site_name% /bindings.[protocol='http://*.80',hostName='%site_name%'] /physicalPath:%site_path% >> %iis_script%
-
 echo %app_cmd% add apppool /name:"%site_name%" /managedRuntimeVersion:"" -processModel.identityType:LocalSystem >> %iis_script%
-echo %app_cmd% add site /name:"%site_name%" /site.name:"%site_name%" /bindings:"http://%site_bindings%:80" /physicalPath:"%site_path%" >> %iis_script%
+echo %app_cmd% add site /name:"%site_name%" /site.name:"%site_name%" /bindings:"http://%site_bindings%:80" /physicalPath:"%physical_path%" >> %iis_script%
 echo %app_cmd% set app "%site_name%/" /applicationPool:"%site_name%" >> %iis_script%
 echo %app_cmd% start apppool /apppool.name:"%site_name%" >> %iis_script%
+echo. >> %iis_script%
 
 exit /b
 
 :create_hosts_section
 
-echo # Site name:        %domain%-%env%%app% >> %hosts_file%
-echo # Physical path:    %site_path% >> %hosts_file%
-echo 127.0.0.1           %site_bindings% >> %hosts_file%
+echo # Site name:        %site_name% >> %hosts_file%
+echo # Application pool: %site_name% >> %hosts_file%
+echo # Physical path:    %physical_path% >> %hosts_file%
+echo 127.0.0.1           %host_name% >> %hosts_file%
 echo. >> %hosts_file%
 
 exit /b
