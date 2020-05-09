7967da3878b381780e7915441aed53a5028195a4
mpab <mpab@asrai.io>
2020-05-09 22:07:10 +0200

    naming convention

--- iis-generate.bat
+++ iis-generate.bat
@@ -1,31 +1,61 @@
 @echo off
 setlocal enabledelayedexpansion
 
+if "%1x"=="x" (
+    echo %0 ERROR: usage %0 iis_script hosts_file
+    exit /b
+)
+
+if "%2x"=="x" (
+    echo %0 ERROR: usage %0 iis_script hosts_file
+    exit /b
+)
+
+if "%solution%x"=="x" (
+    echo %0 ERROR: solution variable not set
+    exit /b
+)
+
+if "%domain%x"=="x" (
+    echo %0 ERROR: domain variable not set
+    exit /b
+)
+
+if "%environment%x"=="x" (
+    echo %0 ERROR: environment variable not set
+    exit /b
+)
+
+if "%webapp%x"=="x" (
+    echo %0 ERROR: webapp variable not set
+    exit /b
+)
+
+if "%iis_solution_physical_path_root%x"=="x" (
+    echo %0 ERROR: iis_solution_physical_path_root variable not set
+    exit /b
+)
+
+set iis_script=%1
+set hosts_file=%2
 set app_cmd=%WINDIR%\system32\inetsrv\appcmd.exe
 
-::set iis_script=scripts\create_iis_sites.bat
-::set domain=example.com
-::set host_name=%domain%
-::set app=TestApi
-::set env=local-
-::call :create_web_site
-
 call :create_web_site_script
-
 call :create_hosts_section
 
 goto :eof
 
 :create_web_site_script
+
 if "%webapp%x"=="uix" (
     set sub_domain=%environment%-%solution%
 ) else (
     set sub_domain=%environment%-%webapp%-%solution%
 )
 
-set site_name=%domain%-%solution%-%webapp%-%environment%
+set site_name=%environment%-%solution%-%webapp%-%domain%
 set host_name=%sub_domain%.%domain%
-set physical_path=C:\inetpub\%domain%-%solution%-%environment%\%vsproject%
+set physical_path=%iis_solution_physical_path_root%\%vsproject%
 
 set site_bindings=%host_name%
 
@@ -42,10 +72,15 @@ exit /b
 
 :create_hosts_section
 
+set info=SOLUTION: %solution%, ENVIRONMENT: %environment%, WEBAPP: %webapp%
+
+echo # %info% >> %hosts_file%
+
 echo # Site name:        %site_name% >> %hosts_file%
 echo # Application pool: %site_name% >> %hosts_file%
 echo # Physical path:    %physical_path% >> %hosts_file%
 echo 127.0.0.1           %host_name% >> %hosts_file%
+
 echo. >> %hosts_file%
 
 exit /b
